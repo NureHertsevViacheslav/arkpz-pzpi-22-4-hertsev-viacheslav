082051d5d042b99fabb0bf59a8d3342b10552712
Viacheslav Hertsev <viacheslav.hertsev@nure.ua>
2025-01-15 18:22:47 +0200

    Update dbConfig.js

--- Task2/arkpz-pzpi-22-4-hertsev-viacheslav-task/dbConfig.js
+++ Task2/arkpz-pzpi-22-4-hertsev-viacheslav-task/dbConfig.js
@@ -1,1 +1,23 @@
+const sql = require('mssql');
 
+// Конфігурація для підключення до бази даних
+const dbConfig = {
+    user: 'your_username',
+    password: 'your_password',
+    server: 'your_server',
+    database: 'your_database',
+    options: {
+        encrypt: true, // Якщо потрібне шифрування
+        trustServerCertificate: true, // Для локального сервера
+    },
+};
+
+const poolPromise = new sql.ConnectionPool(dbConfig)
+    .connect()
+    .then((pool) => {
+        console.log('Connected to MSSQL');
+        return pool;
+    })
+    .catch((err) => console.error('Database Connection Failed!', err));
+
+module.exports = { sql, poolPromise };
